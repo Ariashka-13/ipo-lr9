b5ccebe6e6435b3665d1ed5fad8686fb99705146
Ariashka <ariana0tsarova@gmail.com>
2024-12-02 11:10:29 +0300

    main.py

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ from collision.intersectionAreaMultiRect import intersectionAreaMultiRect
 def main():
     print("Примеры использования isCorrectRect:")
     print(isCorrectRect([(-3.4, 1), (9.2, 10)]))  
-    print(isCorrectRect([(-7, 9), (3, 6)]))       
-
+    print(isCorrectRect([(-7, 9), (3, 6)]))      
+   
     print("Примеры использования isCollisionRect:")
-    print(isCollisionRect([(-3.4, 1), (9.2, 10)], [(-7.4, 0), (13.2, 12)]))  
-    print(isCollisionRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))              
-    print(isCollisionRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]))             
-
+    print(isCollisionRect([(-3.4, 1), (9.2, 10)], [(-7.4, 0), (13.2, 12)])) 
+    print(isCollisionRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))               
+    print(isCollisionRect([(1, 1), (2, 2)], [(1, 3), (3, 4)]))                
+    
     print("Примеры использования intersectionAreaRect:")
     print(intersectionAreaRect([(-3, 1), (9, 10)], [(-7, 0), (13, 12)]))  
-    print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))      
-    print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]))     
-
+    print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))       
+   
     print("Примеры использования intersectionAreaMultiRect:")
     rectangles = [
         [(-3, 1), (9, 10)],
