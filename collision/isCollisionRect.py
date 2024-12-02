8e728afda00964487842ec8ba07d062e6c2d22ce
Ariashka <ariana0tsarova@gmail.com>
2024-12-02 11:20:03 +0300

    Task 3 complete

--- collision/isCollisionRect.py
+++ collision/isCollisionRect.py
@@ -2,9 +2,9 @@ from .isCorrectRect import isCorrectRect
 
 def isCollisionRect(rect1, rect2):
     if not isCorrectRect(rect1): 
-       raise ValueError("1й прямоугольник некоректный") 
+       raise ValueError("1й прямоугольник некоректный.") 
     if not isCorrectRect(rect2): 
-        raise ValueError("2й прямоугольник некоректный") 
+        raise ValueError("2й прямоугольник некоректный.") 
         
     if rect1[1][0] < rect2[0][0]: 
         return False 
